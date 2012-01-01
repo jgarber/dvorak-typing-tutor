e748aebb721285c7253d168f0e08313d06b8d0c6
Jason Garber <jg@jasongarber.com>
2012-01-01 09:56:32 -0500

    Bare rails app with sqlite3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DvorakTypingTutor::Application.config.secret_token = '6bf4c13c659017c48d6c1e9d6c4a13ba6bbceb229a2cd711c80c3f1adbd57301e71bc3c6b263c816a078add1afef9aa197c720f4e6622228843591df01fdd881'
