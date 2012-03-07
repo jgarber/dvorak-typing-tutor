d485134e7e3c49b5e78d2d9c4abff05e6b81ad3d
Max Gonzih <gonzih@gmail.com>
2012-03-07 13:56:51 +0300

    rename module HtmlContent to InputContent
    add some methods for manupulations with ckeditor content

--- features/support/html_content.rb
+++
@@ -1,5 +0,0 @@
-module HtmlContent
-  def html_content
-    page.evaluate_script('app.input_controller.input.stripped_content()')
-  end
-end
---
+++ features/support/input_content.rb
@@ -0,0 +1,23 @@
+module InputContent
+  def html_content
+    page.evaluate_script('app.input_controller.input.get_content();')
+  end
+
+  def get_input_content
+    page.evaluate_script('app.input_controller.input.stripped_content();')
+  end
+
+  def set_input_content(content = '')
+    page.evaluate_script("app.input_controller.input.set_content('#{content}');")
+    trigger_changed
+  end
+
+  def append_input_content(content = '')
+    page.evaluate_script("app.input_controller.input.append_content('#{content}');")
+    trigger_changed
+  end
+
+  def trigger_changed
+    page.execute_script('Spine.trigger("input_box:changed");')
+  end
+end
