836b8cd9ae62c143d49283256aa220af3dede1bb
Jason Garber <jg@jasongarber.com>
2012-03-08 17:24:22 -0500

    Add my copyright

--- vendor/assets/javascripts/ckeditor/plugins/yaspeller/plugin.js
+++ vendor/assets/javascripts/ckeditor/plugins/yaspeller/plugin.js
@@ -1,5 +1,6 @@
 /*
 Copyright (c) 2011 Vitaly Puzrin <vitaly@rcdesign.ru>
+Copyright (c) 2012 Jason Garber <jg@jasongarber.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
